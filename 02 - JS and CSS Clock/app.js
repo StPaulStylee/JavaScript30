e5355d7bf03cb0fd08b18b871a39dadf7b0a861d
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-02-14 14:45:39 -0600

    hour and minute hands function correctly and styled

--- 02 - JS and CSS Clock/app.js
+++ 02 - JS and CSS Clock/app.js
@@ -1,12 +1,21 @@
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
   // take seconds are turn them into a degree
   // we add 90 to offset the oringal location of the hands in the div
-  const currentSecond = ((seconds) / 60 + 360) + 90;
+  const currentSecond = ((seconds / 60) * 360) + 90;
+  const currentMinute = ((minutes / 60) * 360) + 90;
+  const currentHour = ((hours / 12) * 360) + 90;
   secondHand.style.transform = `rotate(${currentSecond}deg)`;
+  minuteHand.style.transform = `rotate(${currentMinute}deg)`;
+  hourHand.style.transform = `rotate(${currentHour}deg)`;
+  console.log(seconds, minutes, hours);
 }
 
 setInterval(setDate, 1000);
