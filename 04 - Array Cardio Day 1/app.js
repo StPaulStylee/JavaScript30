f52929e00fb40cd85488edb768bd5f54215b684f
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-02-16 22:23:39 -0600

    commit

--- 04 - Array Cardio Day 1/app.js
+++ 04 - Array Cardio Day 1/app.js
@@ -22,10 +22,16 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+// This function below filters the inventors array and returns any inventor object that
+// were born in the 1500s.
+const shfifteen = inventors.filter(inventor => inventor.year >= 1500 && inventor.year <= 1599);
+//console.table logs objects into a readable table
+  console.table(shfifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
-
+let fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.log(fullNames);
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 
