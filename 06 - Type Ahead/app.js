4377bee11f387ea3dcdafd04e2a36dbcbe0b8e87
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-02-19 21:34:02 -0600

    fetch used to get city and state data. event listeners built that will display matches.

--- 06 - Type Ahead/app.js
+++ 06 - Type Ahead/app.js
@@ -1,3 +1,27 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
 const cities = [];
+
+fetch(endpoint)
+  .then(rawData => rawData.json())
+  .then(jsonData => cities.push(...jsonData)); // ES6 spread
+
+function locationSearch(locationToFind, cities) {
+  return cities.filter(place => {
+
+    const regex = new RegExp(locationToFind, 'gi'); //g = global; search entire sting & i = case insensitive
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function displayLocations() {
+  const matchedLocation = locationSearch(this.value, cities);
+  console.table(matchedLocation);
+}
+
+
+const searchInput = document.querySelector('.search');
+const searchSuggestions = document. querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayLocations);
+searchInput.addEventListener('keyup', displayLocations);
