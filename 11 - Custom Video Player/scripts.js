59bd14127c7f54580de3e7d66276469d246842c0
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-02-27 20:54:11 -0600

    elements selected and assigned

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,11 @@
+// Get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const play = player.querySelector('.toggle');
+const sliders = document.querySelector('.play__slider');
+const timers = document.querySelector('.player__button');
+
+
+// Build our functions
+
+//Hook up event listeners
