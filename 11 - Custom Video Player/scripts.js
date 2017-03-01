0ddd4ca948f28196fac06afe8ae91abcb7b2fbfe
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-02-28 22:12:46 -0600

    all functionality present except updating progress bar

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,10 +2,44 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const play = player.querySelector('.toggle');
-const sliders = document.querySelector('.play__slider');
-const timers = document.querySelector('.player__button');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const sliders = player.querySelectorAll('.player__slider');
+const timers = player.querySelectorAll('[data-skip]');
 
 
 // Build our functions
 
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  play.textContent = icon;
+}
+
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
 //Hook up event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+play.addEventListener('click', togglePlay);
+
+timers.forEach(timer => timer.addEventListener('click', skip));
+
+sliders.forEach(slider => slider.addEventListener('change', handleRangeUpdate));
+sliders.forEach(slider => slider.addEventListener('mousemove', handleRangeUpdate));
