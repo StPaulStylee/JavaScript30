b85c7c2d34e969d92f1b9a8fba6cfb0f12e5e784
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-03-04 23:42:02 -0600

    checkslide function complete and now the images are sliding in like a boss.

--- 13 - Slide in on Scroll/app.js
+++ 13 - Slide in on Scroll/app.js
@@ -1,3 +1,5 @@
+// debounce takes in a function a waits 20 milliseconds before each iteration of
+// the function call, typically developers want to use this on scroll
 function debounce(func, wait = 20, immediate = true) {
   var timeout;
   return function() {
@@ -12,3 +14,26 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  sliderImages.forEach(slideImage => {
+    // add how much you've scrolled down (from the top) plus the height of the entire window
+    // doing this will simulate dynamic position of the bottom of the window
+    const slideInAt = (window.scrollY + window.innerHeight) - slideImage.height / 2;
+    console.log(slideInAt);
+    // offset top is how for the top of the image is from the top of the window.
+    // adding the height then tells us where the bottom of the image is relation to the window
+    const imageBottom = slideImage.offsetTop + slideImage.height;
+    const isHalfShown = slideInAt > slideImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      slideImage.classList.add('active');
+    } else {
+      slideImage.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
